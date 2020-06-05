2fa9840529e9959336d5fbc20fce000cf9c4839a
Ale <busatto.alessandro96@gmail.com>
2020-06-05 09:54:12 +0200

    Inizio lavoro sulle stringhe dei verificatori

--- exercise_2/verifier/lis.py
+++ exercise_2/verifier/lis.py
@@ -3,8 +3,12 @@ def lis(seq, studseq, n):
     studseq = list(map(int, studseq))
     for i in range(1, n):
         if studseq[i - 1] > studseq[i]:
-            print("Non hai inserito una sequenza crescente")
-            return
+            #print("Non hai inserito una sequenza crescente")
+            min=studseq[i]
+            max=studseq[i - 1]
+            stringa = ("La sequenza che hai inserito non è crescente in quanto " +str(min)+ " < " +str(max)+ " eppure compare dopo di lui.\Si. Totalizzeresti <span style='color:green'>[1 safe pt]</span>, <span style='color:blue'>[9 possible pt]</span>, <span style='color:red'>[0 out of reach pt]</span>.")
+            print(stringa)
+            return stringa
     i = 0
     j = 0
     while i != n and j != len(seq):
@@ -14,10 +18,12 @@ def lis(seq, studseq, n):
         else:
             j += 1
     if i == n:
-        print("Sottosequenza fornita ammissibile: " + str(studseq))
-        print("Bravo/a hai fornito una sottosequenza ammissibile lunga: " + str(n))
+        stringa=("Sottosequenza fornita è un certificato valido: " + str(studseq)+"\Mi hai convinto che la risposta corretta è >= " + str(n))
+        return stringa
     else:
-        print("Sottosequenza fornita sbagliata\n")
+        #print("Sottosequenza fornita sbagliata\n")
+        stringa=("Hai inserito "+str(studseq)+" che non è una sottosequenza di s: "+str(seq)+"  No. Totalizzeresti <span style='color:green'>[0 safe pt]</span>, <span style='color:blue'>[0 possible pt]</span>, <span style='color:red'>[10 out of reach pt]</span>.")
+        return stringa
 
 
 
